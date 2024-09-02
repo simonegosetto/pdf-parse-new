d93770eb97f0b7b8cec07068fd6cfd2d5d5a88ef
Simone Gosetto <simone.g@myvirtualab.it>
2024-09-02 13:26:02 +0200

    fix: avoid dynamic imports

--- index.d.ts
+++ index.d.ts
@@ -3,19 +3,16 @@ export = PdfParse;
 declare function PdfParse(dataBuffer: Buffer, options?: PdfParse.Options): Promise<PdfParse.Result>;
 
 declare namespace PdfParse {
-	// type Version = "default" | "v1.10.100" | "v2.0.550" | "v4.5.136";
 	interface Result {
 		numpages: number;
 		numrender: number;
 		info: any;
 		metadata: any;
-		// version: Version;
 		text: string;
 	}
 	interface Options {
 		pagerender?: ((pageData: any) => string) | undefined;
 		max?: number | undefined;
-		// version?: Version | undefined;
 		verbosityLevel?: 0 | 1 | 5 | undefined;
 	}
 }
