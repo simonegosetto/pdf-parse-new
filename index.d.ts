3db48bf8328a480a1180e87d06267e3468ff91d6
Simone Gosetto <simone.g@myvirtualab.it>
2024-09-02 10:54:38 +0200

    feat: multi-version management removed

--- index.d.ts
+++ index.d.ts
@@ -3,13 +3,13 @@ export = PdfParse;
 declare function PdfParse(dataBuffer: Buffer, options?: PdfParse.Options): Promise<PdfParse.Result>;
 
 declare namespace PdfParse {
-	type Version = "default" | "v1.10.100" | "v2.0.550" | "v4.5.136";
+	// type Version = "default" | "v1.10.100" | "v2.0.550" | "v4.5.136";
 	interface Result {
 		numpages: number;
 		numrender: number;
 		info: any;
 		metadata: any;
-		version: Version;
+		// version: Version;
 		text: string;
 	}
 	interface Options {
